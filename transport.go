c54fbd7d2cac39bf118d47cb21e4a24780b58928
Andy Grunwald <andygrunwald@gmail.com>
2021-03-22 18:57:03 +0100

    Introduce a custom error for refreshToken operation (#45)
    * Introduce a custom error for refreshToken operation
    
    If the refreshToken operation fails (e.g., the github app has been
    susended), the RoundTrip is returning an error. However, the caller
    is not able to figure out the root cause on the failure.
    This commit introduces a custom error that contains the original
    request and response for further inspection.
    
    * Wrap error properly
    
    Co-authored-by: Billy Lynch <wlynch92@gmail.com>
    
    * Remove http.Request from RefreshTokenError
    
    Request is accessible via Response.Request.
    
    * Switched RefreshTokenError to be a pointer receiver
    
    * Moving closing of response body after error casesi
    
    By closing the body late, we provide the caller the chance
    to inspect the body in case of an error.
    
    * Removed duplicate "received non 2xx..." message.
    
    We initiatled the RefreshTokenError type with a default
    error message of receiving a non 2xx response.
    This commit removes the initation message and sets it according
    to the error case.
    
    * Renamed custom error RefreshTokenError -> HTTPError
    
    * Fix typo: operatins -> operations
    
    Co-authored-by: Billy Lynch <wlynch92@gmail.com>
    
    Co-authored-by: Billy Lynch <wlynch92@gmail.com>

--- transport.go
+++ transport.go
@@ -49,6 +49,20 @@ type accessToken struct {
 	Repositories []github.Repository            `json:"repositories,omitempty"`
 }
 
+// HTTPError represents a custom error for failing HTTP operations.
+// Example in our usecase: refresh access token operation.
+// It enables the caller to inspect the root cause and response.
+type HTTPError struct {
+	Message        string
+	RootCause      error
+	InstallationID int64
+	Response       *http.Response
+}
+
+func (e *HTTPError) Error() string {
+	return e.Message
+}
+
 var _ http.RoundTripper = &Transport{}
 
 // NewKeyFromFile returns a Transport using a private key from file.
@@ -116,7 +130,7 @@ func (t *Transport) Token(ctx context.Context) (string, error) {
 	if t.token == nil || t.token.ExpiresAt.Add(-time.Minute).Before(time.Now()) {
 		// Token is not set or expired/nearly expired, so refresh
 		if err := t.refreshToken(ctx); err != nil {
-			return "", fmt.Errorf("could not refresh installation id %v's token: %s", t.installationID, err)
+			return "", fmt.Errorf("could not refresh installation id %v's token: %w", t.installationID, err)
 		}
 	}
 
@@ -164,14 +178,22 @@ func (t *Transport) refreshToken(ctx context.Context) error {
 	t.appsTransport.BaseURL = t.BaseURL
 	t.appsTransport.Client = t.Client
 	resp, err := t.appsTransport.RoundTrip(req)
-	if err != nil {
-		return fmt.Errorf("could not get access_tokens from GitHub API for installation ID %v: %v", t.installationID, err)
+	e := &HTTPError{
+		RootCause:      err,
+		InstallationID: t.installationID,
+		Response:       resp,
 	}
-	defer resp.Body.Close()
+	if err != nil {
+		e.Message = fmt.Sprintf("could not get access_tokens from GitHub API for installation ID %v: %v", t.installationID, err)
+		return e
+	}
 
 	if resp.StatusCode/100 != 2 {
-		return fmt.Errorf("received non 2xx response status %q when fetching %v", resp.Status, req.URL)
+		e.Message = fmt.Sprintf("received non 2xx response status %q when fetching %v", resp.Status, req.URL)
+		return e
 	}
+	// Closing body late, to provide caller a chance to inspect body in an error / non-200 response status situation
+	defer resp.Body.Close()
 
 	return json.NewDecoder(resp.Body).Decode(&t.token)
 }
