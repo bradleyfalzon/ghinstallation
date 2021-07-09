0dfa7e21ab11fe4dfd82f54a736c1f8483fcc66e
Andrew Svoboda <asvoboda@palantir.com>
2021-07-09 09:41:34 +0100

    Update go-github to v0.37.0

--- transport.go
+++ transport.go
@@ -11,7 +11,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/google/go-github/v35/github"
+	"github.com/google/go-github/v37/github"
 )
 
 const (
--- transport_test.go
+++ transport_test.go
@@ -12,7 +12,7 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v35/github"
+	"github.com/google/go-github/v37/github"
 )
 
 const (
